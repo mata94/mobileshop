b02bb353bf8f1ef05360dd9915a5987a4dc7ede8
franpavlovi <fljubuski@gmail.com>
2024-02-13 19:26:12 +0100

    fix

--- src/main/java/com/example/onlineshop/OnlineshopApplication.java
+++ src/main/java/com/example/onlineshop/OnlineshopApplication.java
@@ -1,5 +1,9 @@
 package com.example.onlineshop;
 
+import com.example.onlineshop.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
