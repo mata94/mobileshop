f49d2d2fbaacdc1de6e9a0c7ba6033b64527cb3d
mata94 <golemac14@gmail.com>
2024-02-13 18:54:24 +0100

    orders

--- src/main/java/com/example/onlineshop/OnlineshopApplication.java
+++ src/main/java/com/example/onlineshop/OnlineshopApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineshopApplication {
+public class OnlineshopApplication implements ApplicationRunner {
 
+	@Autowired
+	private UserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineshopApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) {
+		userService.adminRegistration();
+	}
 }
